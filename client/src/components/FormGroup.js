7438bd8f32e7be7d8d8e3cf6f3e610ef4390d1be
Kelvin Porter <kporter@protonmail.com>
2020-04-13 19:41:44 -0400

    Remove useless constructor

--- client/src/components/FormGroup.js
+++ client/src/components/FormGroup.js
@@ -1,9 +1,6 @@
 import React, { Component } from "react";
 
 class FormGroup extends Component {
-  constructor(props) {
-    super(props);
-  }
   render() {
     return (
       <div className="form-group">
