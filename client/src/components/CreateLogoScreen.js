9792815428af36e4edc85f485e78f648e38cb434
Kelvin Porter <kporter@protonmail.com>
2020-04-13 06:37:46 -0400

    CurrentLogo and Card to CreateLogoScreen

--- client/src/components/CreateLogoScreen.js
+++ client/src/components/CreateLogoScreen.js
@@ -3,6 +3,7 @@ import gql from "graphql-tag";
 import { Mutation } from "react-apollo";
 import { Link } from "react-router-dom";
 import FormGroup from "./FormGroup";
+import CurrentLogo from "./CurrentLogo";
 
 const ADD_LOGO = gql`
   mutation AddLogo(
@@ -55,122 +56,141 @@ class CreateLogoScreen extends Component {
       >
         {(addLogo, { loading, error }) => (
           <div className="container">
-            <div className="panel panel-default">
-              <div className="panel-heading">
-                <h4>
-                  <Link to="/">Home</Link>
-                </h4>
-                <h3 className="panel-title">Create Logo</h3>
+            <div className="row">
+              <div className="col-4">
+                <div className="card">
+                  <div className="card-header">
+                    <h4>
+                      <Link to="/">Home</Link>
+                    </h4>
+                    <h3 className="card-title">Create Logo</h3>
+                  </div>
+                  <div className="card-body">
+                    <form
+                      onSubmit={(e) => {
+                        e.preventDefault();
+                        addLogo({
+                          variables: {
+                            text: this.state.text,
+                            color: this.state.color,
+                            fontSize: parseInt(this.state.fontSize),
+                            backgroundColor: this.state.backgroundColor,
+                            borderColor: this.state.borderColor,
+                            borderRadius: parseInt(this.state.borderRadius),
+                            borderWidth: parseInt(this.state.borderWidth),
+                            padding: parseInt(this.state.padding),
+                            margin: parseInt(this.state.margin),
+                          },
+                        });
+                      }}
+                    >
+                      <FormGroup
+                        htmlFor="text"
+                        param="text"
+                        label="Text"
+                        formType="text"
+                        defValue={this.state.text}
+                        onChangeCallback={(e) => {
+                          this.setState({ text: e.target.value });
+                        }}
+                      />
+                      <FormGroup
+                        param="color"
+                        label="Color"
+                        formType="color"
+                        defValue={this.state.color}
+                        onChangeCallback={(e) => {
+                          this.setState({ color: e.target.value });
+                        }}
+                      />
+                      <FormGroup
+                        param="fontSize"
+                        label="Font Size"
+                        formType="text"
+                        defValue={this.state.fontSize}
+                        onChangeCallback={(e) => {
+                          this.setState({ fontSize: e.target.value });
+                        }}
+                      />
+                      <FormGroup
+                        param="backgroundColor"
+                        label="Background Color"
+                        formType="color"
+                        defValue={this.state.backgroundColor}
+                        onChangeCallback={(e) => {
+                          this.setState({
+                            backgroundColor: e.target.value,
+                          });
+                        }}
+                      />
+                      <FormGroup
+                        param="borderColor"
+                        label="Border Color"
+                        formType="color"
+                        defValue={this.state.borderColor}
+                        onChangeCallback={(e) => {
+                          this.setState({ borderColor: e.target.value });
+                        }}
+                      />
+                      <FormGroup
+                        param="borderRadius"
+                        label="Border Radius"
+                        formType="text"
+                        defValue={this.state.borderRadius}
+                        onChangeCallback={(e) => {
+                          this.setState({ borderRadius: e.target.value });
+                        }}
+                      />
+                      <FormGroup
+                        param="borderWidth"
+                        label="Border Width"
+                        formType="text"
+                        defValue={this.state.borderWidth}
+                        onChangeCallback={(e) => {
+                          this.setState({ borderWidth: e.target.value });
+                        }}
+                      />
+                      <FormGroup
+                        param="padding"
+                        label="Border Padding"
+                        formType="text"
+                        defValue={this.state.padding}
+                        onChangeCallback={(e) => {
+                          this.setState({ padding: e.target.value });
+                        }}
+                      />
+                      <FormGroup
+                        param="margin"
+                        label="Border Margin"
+                        formType="text"
+                        defValue={this.state.margin}
+                        onChangeCallback={(e) => {
+                          this.setState({ margin: e.target.value });
+                        }}
+                      />
+                      <button type="submit" className="btn btn-success">
+                        Submit
+                      </button>
+                    </form>
+                    {loading && <p>Loading...</p>}
+                    {error && <p>Error :( Please try again</p>}
+                  </div>
+                </div>
               </div>
-              <div className="panel-body">
-                <form
-                  onSubmit={(e) => {
-                    e.preventDefault();
-                    addLogo({
-                      variables: {
-                        text: this.state.text,
-                        color: this.state.color,
-                        fontSize: parseInt(this.state.fontSize),
-                        backgroundColor: this.state.backgroundColor,
-                        borderColor: this.state.borderColor,
-                        borderRadius: parseInt(this.state.borderRadius),
-                        borderWidth: parseInt(this.state.borderWidth),
-                        padding: parseInt(this.state.padding),
-                        margin: parseInt(this.state.margin),
-                      },
-                    });
+              <div className="col-8">
+                <CurrentLogo
+                  logo={{
+                    text: this.state.text,
+                    color: this.state.color,
+                    fontSize: this.state.fontSize,
+                    backgroundColor: this.state.backgroundColor,
+                    borderColor: this.state.borderStyle,
+                    borderRadius: this.state.borderRadius,
+                    borderWidth: this.state.borderWidth,
+                    padding: this.state.padding,
+                    margin: this.state.margin,
                   }}
-                >
-                  <FormGroup
-                    htmlFor="text"
-                    param="text"
-                    label="Text"
-                    formType="text"
-                    defValue={this.state.text}
-                    onChangeCallback={(e) => {
-                      this.setState({ text: e.target.value });
-                    }}
-                  />
-                  <FormGroup
-                    param="color"
-                    label="Color"
-                    formType="color"
-                    defValue={this.state.color}
-                    onChangeCallback={(e) => {
-                      this.setState({ color: e.target.value });
-                    }}
-                  />
-                  <FormGroup
-                    param="fontSize"
-                    label="Font Size"
-                    formType="text"
-                    defValue={this.state.fontSize}
-                    onChangeCallback={(e) => {
-                      this.setState({ fontSize: e.target.value });
-                    }}
-                  />
-                  <FormGroup
-                    param="backgroundColor"
-                    label="Background Color"
-                    formType="color"
-                    defValue={this.state.backgroundColor}
-                    onChangeCallback={(e) => {
-                      this.setState({
-                        backgroundColor: e.target.value,
-                      });
-                    }}
-                  />
-                  <FormGroup
-                    param="borderColor"
-                    label="Border Color"
-                    formType="color"
-                    defValue={this.state.borderColor}
-                    onChangeCallback={(e) => {
-                      this.setState({ borderColor: e.target.value });
-                    }}
-                  />
-                  <FormGroup
-                    param="borderRadius"
-                    label="Border Radius"
-                    formType="text"
-                    defValue={this.state.borderRadius}
-                    onChangeCallback={(e) => {
-                      this.setState({ borderRadius: e.target.value });
-                    }}
-                  />
-                  <FormGroup
-                    param="borderWidth"
-                    label="Border Width"
-                    formType="text"
-                    defValue={this.state.borderWidth}
-                    onChangeCallback={(e) => {
-                      this.setState({ borderWidth: e.target.value });
-                    }}
-                  />
-                  <FormGroup
-                    param="padding"
-                    label="Border Padding"
-                    formType="text"
-                    defValue={this.state.padding}
-                    onChangeCallback={(e) => {
-                      this.setState({ padding: e.target.value });
-                    }}
-                  />
-                  <FormGroup
-                    param="margin"
-                    label="Border Margin"
-                    formType="text"
-                    defValue={this.state.margin}
-                    onChangeCallback={(e) => {
-                      this.setState({ margin: e.target.value });
-                    }}
-                  />
-                  <button type="submit" className="btn btn-success">
-                    Submit
-                  </button>
-                </form>
-                {loading && <p>Loading...</p>}
-                {error && <p>Error :( Please try again</p>}
+                />
               </div>
             </div>
           </div>
