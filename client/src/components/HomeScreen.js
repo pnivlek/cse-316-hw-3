c4fceade7d6bbfd6674a18a0a86eeabd94653c6b
Kelvin Porter <kporter@protonmail.com>
2020-04-13 18:45:57 -0400

    Make the home page prettier.

--- client/src/components/HomeScreen.js
+++ client/src/components/HomeScreen.js
@@ -23,49 +23,52 @@ class HomeScreen extends Component {
           if (error) return `Error! ${error.message}`;
 
           return (
-            <div className="container row">
-              <div className="col s4">
-                <div className="card">
-                  <div className="card-header">
-                    <h4 className="card-title">Recent Work</h4>
+            <div className="container">
+              <div className="row">
+                <div className="col-8">
+                  <div className="card">
+                    <div className="card-header">
+                      <div id="home_banner_container">goLogoLo Logo Maker</div>
+                    </div>
+                    <div className="card-body">
+                      <Link
+                        id="add_logo_button"
+                        to="/create"
+                        className="btn btn-primary"
+                      >
+                        Add Logo
+                      </Link>
+                    </div>
                   </div>
-                  <div className="card-body">
-                    {data.logos
-                      .sort(
-                        (logo, logo2) =>
-                          new Date(logo2.lastUpdate) - new Date(logo.lastUpdate)
-                      )
-                      .map((logo, index) => (
-                        <div
-                          key={index}
-                          className="home_logo_link"
-                          style={{ cursor: "pointer" }}
-                        >
-                          <Link
-                            className="btn btn-primary"
-                            to={`/view/${logo._id}`}
+                </div>
+                <div className="col s4">
+                  <div className="card">
+                    <div className="card-header">
+                      <h4 className="card-title">Recent Work</h4>
+                    </div>
+                    <div className="card-body">
+                      {data.logos
+                        .sort(
+                          (logo, logo2) =>
+                            new Date(logo2.lastUpdate) -
+                            new Date(logo.lastUpdate)
+                        )
+                        .map((logo, index) => (
+                          <div
+                            key={index}
+                            className="home_logo_link"
+                            style={{ cursor: "pointer" }}
                           >
-                            {logo.text}
-                          </Link>
-                        </div>
-                      ))}
+                            <Link
+                              className="btn btn-secondary"
+                              to={`/view/${logo._id}`}
+                            >
+                              {logo.text}
+                            </Link>
+                          </div>
+                        ))}
+                    </div>
                   </div>
-                </div>
-              </div>
-              <div className="col-8">
-                <div id="home_banner_container">
-                  goLogoLo
-                  <br />
-                  Logo Maker
-                </div>
-                <div>
-                  <Link
-                    id="add_logo_button"
-                    to="/create"
-                    className="btn btn-primary"
-                  >
-                    Add Logo
-                  </Link>
                 </div>
               </div>
             </div>
